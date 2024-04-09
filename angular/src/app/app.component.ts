ee1e5529e84bf93e1bdff979c878e2d87c2d7b32
luigiguajana93 <luigi.guajana@gmail.com>
2024-04-09 12:08:06 +0200

    committino nuovo

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BgcolorService } from './services/bgcolor.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  isDark:boolean = true
+  render: any;
+  backgroundColor: string;
+
+constructor(private colorSvc:BgcolorService){
+
+  this.backgroundColor = this.colorSvc.getBackgroundColor();
+}
+  changecolor(){
+    if(this.isDark){
+      this.render.setStyle(document.body,'background-color','#212121');
+}
+else{
+this.render.setStyle(document.body,'background-color','#fff')
+}
+this.isDark = !this.isDark;
+}
 }
--- angular/src/app/mainComponents/header/header.component.ts
+++ angular/src/app/mainComponents/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { BgcolorService } from '../../services/bgcolor.service';
 
 @Component({
   selector: 'app-header',
@@ -12,15 +13,27 @@ export class HeaderComponent {
 
   isUserLoggedIn:boolean = false
 
-  constructor(private authSvc:AuthService){}
+
+  constructor(private authSvc:AuthService,private colorSvc:BgcolorService){}
 
   ngOnInit(){
     this.authSvc.$isLoggedIn.subscribe(data => {
       this.isUserLoggedIn = data;
     })
+
+
+
   }
 
   logout():void{
     this.authSvc.logout()
   }
+
+  changeColor() {
+    this.colorSvc.setBackgroundColor();
+
+}
+
+
+
 }
